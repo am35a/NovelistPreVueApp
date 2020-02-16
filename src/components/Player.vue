491af4b719b6f7148185d881819334d93f0f12fa
Arthur A. Selimov <am35a.piston@gmail.com>
2020-02-16 23:24:41 +0300

    recreated components and alived playerShare

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,116 +1,34 @@
 <template>
-    <div class="player-container position-fixed overflow-hidden bg-light l-0 b-0 flex-column text-black-75">
+    <div class="player-container position-fixed overflow-hidden bg-light l-0 b-0 text-black-75">
 		<div class="player-container-bkg position-absolute l-0 t-0 w-100 h-100" v-bind:style="{ 'background-image' : 'url(' + 'http://mobitoon.ru/novelist/images/books/' + playBook.bookIndex.id + '/preview.jpg' + ')'}"></div>
-<!-- start | player main container -->
-<!--    start | palyer nav -->
-        <div class="d-flex flex-row">
-            <div class="d-flex justify-content-between w-50">
-                <svg class="wh-18p p-3 content-box" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M352 320c-22.608 0-43.387 7.819-59.79 20.895l-102.486-64.054a96.551 96.551 0 0 0 0-41.683l102.486-64.054C308.613 184.181 329.392 192 352 192c53.019 0 96-42.981 96-96S405.019 0 352 0s-96 42.981-96 96c0 7.158.79 14.13 2.276 20.841L155.79 180.895C139.387 167.819 118.608 160 96 160c-53.019 0-96 42.981-96 96s42.981 96 96 96c22.608 0 43.387-7.819 59.79-20.895l102.486 64.054A96.301 96.301 0 0 0 256 416c0 53.019 42.981 96 96 96s96-42.981 96-96-42.981-96-96-96z"></path></svg>
-                <svg class="wh-18p p-3 content-box" v-bind:class="classStarsColor" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-                <svg class="wh-18p p-3 content-box" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><path fill="currentColor" d="M20 424.229h20V279.771H20c-11.046 0-20-8.954-20-20V212c0-11.046 8.954-20 20-20h112c11.046 0 20 8.954 20 20v212.229h20c11.046 0 20 8.954 20 20V492c0 11.046-8.954 20-20 20H20c-11.046 0-20-8.954-20-20v-47.771c0-11.046 8.954-20 20-20zM96 0C56.235 0 24 32.235 24 72s32.235 72 72 72 72-32.235 72-72S135.764 0 96 0z"></path></svg>
-                <svg class="wh-18p p-3 content-box" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M80 368H16a16 16 0 0 0-16 16v64a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16v-64a16 16 0 0 0-16-16zm0-320H16A16 16 0 0 0 0 64v64a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16V64a16 16 0 0 0-16-16zm0 160H16a16 16 0 0 0-16 16v64a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16v-64a16 16 0 0 0-16-16zm416 176H176a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm0-320H176a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16V80a16 16 0 0 0-16-16zm0 160H176a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16z"></path></svg>
-            </div>
-            <div class="ml-auto d-flex">
-                <svg class="wh-18p p-3 content-box" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg>
-                <svg v-on:click="closePlayer" class="wh-18p p-3 content-box" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>                
-            </div>
-        </div>
-<!--    end | palyer nav -->
-<!--    start | player book controls -->
-        <div class="my-auto px-3">
-<!--        start | book's range and speed controls -->
-            <div class="position-relative">
-                <input class="custom-range player-custom-range position-absolute" type="range" v-model="playBook.bookIndex.listened" min="0" max="100">
-                <div class="progress bg-white" style="height: 1px;">
-                    <div class="progress-bar bg-success" role="progressbar" v-bind:style="{width: playBook.bookIndex.listened + '%'}"></div>
-                </div>
-            </div>
-			<div class="d-flex justify-content-between align-items-start">
-				<small>10092</small>
-				<div class="position-relative mt-1">
-					<small class="position-absolute mt-n1 t-0 l-0 w-100 text-center"><small>Normal</small></small>
-					<svg class="wh-24p p-3 content-box" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M288 32C128.94 32 0 160.94 0 320c0 52.8 14.25 102.26 39.06 144.8 5.61 9.62 16.3 15.2 27.44 15.2h443c11.14 0 21.83-5.58 27.44-15.2C561.75 422.26 576 372.8 576 320c0-159.06-128.94-288-288-288zm0 64c14.71 0 26.58 10.13 30.32 23.65-1.11 2.26-2.64 4.23-3.45 6.67l-9.22 27.67c-5.13 3.49-10.97 6.01-17.64 6.01-17.67 0-32-14.33-32-32S270.33 96 288 96zM96 384c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm48-160c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm246.77-72.41l-61.33 184C343.13 347.33 352 364.54 352 384c0 11.72-3.38 22.55-8.88 32H232.88c-5.5-9.45-8.88-20.28-8.88-32 0-33.94 26.5-61.43 59.9-63.59l61.34-184.01c4.17-12.56 17.73-19.45 30.36-15.17 12.57 4.19 19.35 17.79 15.17 30.36zm14.66 57.2l15.52-46.55c3.47-1.29 7.13-2.23 11.05-2.23 17.67 0 32 14.33 32 32s-14.33 32-32 32c-11.38-.01-20.89-6.28-26.57-15.22zM480 384c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32z"></path></svg>
-				</div>
-				<small>43200</small>
-			</div>
-<!--        end | book's range and speed controls -->
-<!--        start | book's cover -->
-            <div class="px-4 text-center">
-                <svg slot="bookCoverPreview" class="img-fluid rounded-lg shadow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="512" height="512">
-                    <title>{{ playBook.bookIndex.title + ' by ' + playBook.bookIndex.author }}</title>
-                    <defs>
-                        <pattern id="bookCover" patternUnits="userSpaceOnUse" width="512" height="512">
-                            <image v-bind:xlink:href="'http://mobitoon.ru/novelist/images/books/' + playBook.bookIndex.id + '/preview.jpg'" x="0" y="0" width="512" height="512"/>
-                        </pattern>
-                    </defs>
-                    <rect width="512" height="512" fill="url(#bookCover)"></rect>
-                </svg>
-            </div>
-<!--        end | book's cover -->
-<!--        start | book's chapters list controls -->
-            <div class="d-flex justify-content-between align-items-center">
-                <div>
-                    <svg class="wh-24p p-4 content-box" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 504C119 504 8 393 8 256S119 8 256 8s248 111 248 248-111 248-248 248zM142.1 273l135.5 135.5c9.4 9.4 24.6 9.4 33.9 0l17-17c9.4-9.4 9.4-24.6 0-33.9L226.9 256l101.6-101.6c9.4-9.4 9.4-24.6 0-33.9l-17-17c-9.4-9.4-24.6-9.4-33.9 0L142.1 239c-9.4 9.4-9.4 24.6 0 34z"></path></svg>
-                </div>
-                <div class="text-truncate">Title of a chapter lorem</div>
-                <div>
-                    <svg class="wh-24p p-4 content-box" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 8c137 0 248 111 248 248S393 504 256 504 8 393 8 256 119 8 256 8zm113.9 231L234.4 103.5c-9.4-9.4-24.6-9.4-33.9 0l-17 17c-9.4 9.4-9.4 24.6 0 33.9L285.1 256 183.5 357.6c-9.4 9.4-9.4 24.6 0 33.9l17 17c9.4 9.4 24.6 9.4 33.9 0L369.9 273c9.4-9.4 9.4-24.6 0-34z"></path></svg>
-                </div>
-            </div>
-<!--        end | book's chapters list controls -->
-<!--        start | book's play controls -->
-            <div class="d-flex justify-content-center align-items-center">
-                <div class="p-3 mt-4 text-center">
-                    <svg class="wh-42p" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M11.5 280.6l192 160c20.6 17.2 52.5 2.8 52.5-24.6V96c0-27.4-31.9-41.8-52.5-24.6l-192 160c-15.3 12.8-15.3 36.4 0 49.2zm256 0l192 160c20.6 17.2 52.5 2.8 52.5-24.6V96c0-27.4-31.9-41.8-52.5-24.6l-192 160c-15.3 12.8-15.3 36.4 0 49.2z"></path></svg>
-                    <div><sup>-100 sig</sup></div>
-                </div>
-                <div class="mx-3">
-                    <svg class="wh-54p p-4 content-box" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path></svg>
-                </div>
-                <div class="p-3 mt-4 text-center">
-                    <svg class="wh-42p" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M500.5 231.4l-192-160C287.9 54.3 256 68.6 256 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2zm-256 0l-192-160C31.9 54.3 0 68.6 0 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2z"></path></svg>
-                    <div><sup>+100 sig</sup></div>
-                </div>
-            </div>
-<!--        end | book's play controls -->
-        </div>
-<!--    end | player book controls -->
-<!-- end | player main container -->
-		<PlayerShare v-show="isShareShow"></PlayerShare>
-        <PlayerVote v-show="isVoteShow"></PlayerVote>
+		<player-home v-bind:class="{ 'd-flex' : isHomeShow, 'd-none' : !isHomeShow }"></player-home>
+		<player-share v-bind:class="{ 'd-flex' : isShareShow, 'd-none' : !isShareShow }"></player-share>
+        <player-vote v-bind:class="{ 'd-flex' : isVoteShow, 'd-none' : !isVoteShow }"></player-vote>
     </div>
 </template>
 
 <script>
+    import PlayerHome from './PlayerHome.vue'
     import PlayerShare from './PlayerShare.vue'
     import PlayerVote from './PlayerVote.vue'
 
     export default {
-        // props:[
-        // ],
+        data(){
+            return{
+                isHomeShow: false,
+                isShareShow: true,
+                isVoteShow: false
+            }
+        },
         inject: [
             'playBook'
         ],
-        data(){
-            return{
-                isShareShow: false,
-                isVoteShow: false,
-                classStarsColor: {
-                    'text-danger': this.playBook.bookIndex.stars == 1,
-                    'text-warning': this.playBook.bookIndex.stars > 1 && this.playBook.bookIndex.stars < 5,
-                    'text-success': this.playBook.bookIndex.stars == 5
-                }
-            }
-        },
         // watch: {
         // },
         methods: {
-            closePlayer(){
-                this.$parent.isShowFullPlayer = false
-            }
-
         },
         components: {
+            PlayerHome,
             PlayerShare,
             PlayerVote
         }
@@ -133,9 +51,4 @@
         z-index: -1;
         opacity: .75;
     }
-	.player-custom-range {
-		transform: translateY(-50%);
-		opacity: 0;
-		z-index: 1;
-	}
 </style>
--- src/components/PlayerShare.vue
+++ src/components/PlayerShare.vue
@@ -1,6 +1,16 @@
 <template>
-    <div>
-        share main container
+    <div class="flex-column l-0 t-0 w-100 h-100">
+
+        <!-- s | top menu -->
+        <div class="d-flex">
+            <small class="my-auto pl-3 text-uppercase font-weight-bolder">Share the book</small>
+            <svg v-on:click="closePlayerShare" class="wh-18p ml-auto p-3 content-box" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>                
+        </div>
+        <!-- e | top menu -->
+
+        <div class="my-auto px-3">
+            ...
+        </div>
     </div>
 </template>
 
@@ -13,7 +23,13 @@
         },
         inject: [
             'playBook'
-        ]
+        ],
+        methods: {
+            closePlayerShare(){
+                this.$parent.isHomeShow = true
+                this.$parent.isShareShow = false
+            }
+        }
     }
 </script>
 
